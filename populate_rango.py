8eae4a33bbf68e91e8a004d3d3253b99f55d3be8
Linkun Ye <2708082y@student.gla.ac.uk>
2023-02-06 18:43:52 +0000

    copy

--- populate_rango.py
+++ populate_rango.py
@@ -7,34 +7,34 @@ from rango.models import Category,Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/','views':32},
         {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/','views':321},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/','views':3} ]
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/','views':3333},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/','views':7},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+         'url':'http://www.tangowithdjango.com/','views':7222} ]
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/','views':71},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+         'url':'http://flask.pocoo.org','views':90} ]
     cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
             'Django': {'pages': django_pages,'views':64,'likes':32},
             'Other Frameworks': {'pages': other_pages,'views':64,'likes':32} }
     for cat,cat_data in cats.items():
         c= add_cat(cat,cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c,p['title'],p['url'])
+            add_page(c,p['title'],p['url'],p['views'])
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,84 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from django.shortcuts import redirect
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Linkun Ye'}
     return render(request, 'rango/about.html', context=context_dict)
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})    
+def add_page(request,category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    if category is None:
+        return redirect('/rango/')
+        
+        
+
+    form = PageForm()
+    
+    
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                form.save(commit=True)
+                page.category = category
+                page.views = 0
+                page.save()
+                
+
+                return redirect(reverse('rango:show_category',
+                                            kwargs={'category_name_slug':
+                                                    category_name_slug}))
+
+        else:
+             print(form.errors)
+        context_dict = {'form': form, 'category': category}
+
+    
+    return render(request, 'rango/add_page.html',context=context_dict)  
+
+
+
+
+
+
+    
+
+
+
+
+
